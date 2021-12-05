9f056fbc487c8f1dbd453aad4e77f4efef10c1d3
Jiaqi-Xu99 <xu.4015@osu.edu>
2021-12-05 17:44:00 -0500

    Project page is done

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :class_session, foreign_key: "class_session_id"
   has_many :evaluations
+  
+  validates :name, presence: {message: 'Name needed'}
 end
