6882dc689ee98568e5f35d2cbe8097d78010f368
henguyen99 <henguyen99@gmail.com>
2021-12-05 16:26:41 -0500

    Choose user type in user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
      @user = User.new(user_params)
      if @user.save
        log_in @user
-       flash[:success] = "Welcome to the Sample App!"
+       flash[:success] = "Successfully created your profile!"
        redirect_to @user
      else
        render 'new'
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
    private
      def user_params
        params.require(:user).permit(:first_name, :last_name, :email, :password,
-                                    :password_confirmation)
+                                    :password_confirmation, :user_type)
      end
      # Before filters
  
